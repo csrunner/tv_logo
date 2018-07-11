ca61ed9d20cdea4ccdd315cf2e2e81d737b97218
csrunner <chaoshi_cn@hotmail.com>
2018-07-11 19:41:57 +0800

    add std to frame difference

--- video_frequency.py
+++ video_frequency.py
@@ -9,6 +9,7 @@ import glob
 import cv2
 import os
 import tensorflow as tf
+import time
 
 def readin_image(readin_path):
     img_paths = glob.glob(os.path.join(readin_path,'*.jpeg'))
@@ -30,7 +31,7 @@ def low_pass(flatten_array,img_stack,dimx,dimy,use_flatten=True):
         low_pass_video_sig = np.zeros(flatten_array.shape)
         for i in range(dimx*dimy):
 
-            sig_tmp = flatten_array[i,:]*np.random.random([flatten_array.shape[1],])
+            sig_tmp = flatten_array[i,:]
             sig = []
             for j in range(len(sig_tmp)):
                 sig.append(float(sig_tmp[j]))
@@ -51,6 +52,7 @@ def frame_diff(flatten_img_stack,img_stack,use_flatten=False):
         for i in range(1,dimy):
             this = flatten_img_stack[:,i]
             flatten_img_stack[:,i-1] = this-previous
+            previous = this
         return flatten_diff_stack
     else:
         [dimx,dimy,dimz] = img_stack.shape
@@ -65,71 +67,110 @@ def frame_diff(flatten_img_stack,img_stack,use_flatten=False):
 
 
 def img_freq_analysis():
-    PLT_SHOW = True
-    readin_path = '/Users/shichao/Downloads'
-    [flatten_img_stack, img_stack, dimx, dimy, img_num] = readin_image(readin_path)
-    diff_stack = frame_diff(flatten_img_stack,img_stack,use_flatten=False)
-    difference_sum = np.sum(diff_stack,axis=2)
-    frame_diff_dst = np.zeros(difference_sum.shape)
-    cv2.normalize(difference_sum, frame_diff_dst, norm_type=cv2.NORM_MINMAX)
-    frame_diff_seq_std_norm_255 = (255 * frame_diff_dst).astype(np.uint8)
-    ret, frame_diff_seq_std_norm_thresh = cv2.threshold(frame_diff_seq_std_norm_255, 0, 255, cv2.THRESH_OTSU)
-    # seq_std_norm_thresh = cv2.adaptiveThreshold(seq_std_norm_255, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-    #                                             cv2.THRESH_BINARY_INV, 11, 2)
-    # seq_std_norm_thresh = cv2.adaptiveThreshold(seq_std_norm_255, 255, cv2.ADAPTIVE_THRESH_MEAN_C,
-    #                                             cv2.THRESH_BINARY_INV, 11, 2)
-    frame_diff_seq_std_norm_thresh_inv = 255 - frame_diff_seq_std_norm_thresh
 
-    # use standivation
-    seq_std = diff_stack.std(axis=2)
-    dst = np.zeros(difference_sum.shape)
-    cv2.normalize(seq_std, dst, cv2.NORM_MINMAX)
-    seq_std_norm_255 = (255 * dst).astype(np.uint8)
-    ret, seq_std_norm_thresh = cv2.threshold(seq_std_norm_255, 0, 255, cv2.THRESH_OTSU)
-    seq_std_norm_thresh_inv = 255 - seq_std_norm_thresh
-
-    non_zero_frameDiff_ratio = len(np.nonzero(frame_diff_seq_std_norm_thresh_inv)[0])/float(dimx*dimy)
-    non_zero_std_ratio = len(np.nonzero(seq_std_norm_thresh_inv)[0])/float(dimx*dimy)
-    print('frame difference non zero ratio: {0}'.format(non_zero_frameDiff_ratio))
-    print('std non zero ratio: {0}'.format(non_zero_std_ratio))
+    PLOT_IMAGE = False
+    if PLOT_IMAGE:
+        PLT_SHOW = True
+        readin_path = '/Users/shichao/Downloads'
+        [flatten_img_stack, img_stack, dimx, dimy, img_num] = readin_image(readin_path)
+        diff_start = time.time()
+        diff_stack = frame_diff(flatten_img_stack,img_stack,use_flatten=False)
+        difference_sum = np.sum(diff_stack,axis=2)
+        frame_diff_dst = np.zeros(difference_sum.shape)
+        cv2.normalize(difference_sum, frame_diff_dst, norm_type=cv2.NORM_MINMAX)
+        frame_diff_seq_std_norm_255 = (255 * frame_diff_dst).astype(np.uint8)
+        ret, frame_diff_seq_std_norm_thresh = cv2.threshold(frame_diff_seq_std_norm_255, 0, 255, cv2.THRESH_OTSU)
+        # seq_std_norm_thresh = cv2.adaptiveThreshold(seq_std_norm_255, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+        #                                             cv2.THRESH_BINARY_INV, 11, 2)
+        # seq_std_norm_thresh = cv2.adaptiveThreshold(seq_std_norm_255, 255, cv2.ADAPTIVE_THRESH_MEAN_C,
+        #                                             cv2.THRESH_BINARY_INV, 11, 2)
+        frame_diff_seq_std_norm_thresh_inv = 255 - frame_diff_seq_std_norm_thresh
+        diff_end = time.time()
+        diff_time = diff_end-diff_start
 
 
+        # use standivation
+        std_start = time.time()
+        seq_std = img_stack.std(axis=2)
+        dst = np.zeros(img_stack.shape[:2])
+        cv2.normalize(seq_std, dst, cv2.NORM_MINMAX)
+        seq_std_norm_255 = (255 * dst).astype(np.uint8)
+        ret, seq_std_norm_thresh = cv2.threshold(seq_std_norm_255, 0, 255, cv2.THRESH_OTSU)
+        seq_std_norm_thresh_inv = 255 - seq_std_norm_thresh
+        std_end = time.time()
+        std_time = std_end-std_start
 
-    if PLT_SHOW:
+        non_zero_frameDiff_ratio = len(np.nonzero(frame_diff_seq_std_norm_thresh_inv)[0])/float(dimx*dimy)
+        non_zero_std_ratio = len(np.nonzero(seq_std_norm_thresh_inv)[0])/float(dimx*dimy)
+        print('frame difference non zero ratio: {0} with elapsed time {1}'.format(non_zero_frameDiff_ratio,diff_time))
+        print('std non zero ratio: {0} with elapsed time {1}'.format(non_zero_std_ratio,std_time))
+
+
+        if PLT_SHOW:
+            plt.subplot(221)
+            # plt.imshow(seq_std_norm_thresh_inv,cmap='jet')
+            plt.imshow(frame_diff_seq_std_norm_thresh_inv)
+            plt.title('sum of frame difference_otsu')
+
+            plt.subplot(222)
+            plt.imshow(difference_sum)
+            plt.title('sum of frame difference')
+
+
+
+            plt.subplot(223)
+            plt.imshow(seq_std_norm_thresh_inv)
+            plt.title('standard devivation_otsu')
+
+            plt.subplot(224)
+            plt.imshow(seq_std)
+            plt.title('standard devivation')
+
+
+            plt.show()
+        else:
+            cv2.imshow('difference',seq_std_norm_thresh_inv)
+            cv2.waitKey(5000)
+            cv2.destroyWindow('difference')
+
+
+    else:
+        readin_path = '/Users/shichao/Downloads'
+        [flatten_img_stack, img_stack, dimx, dimy, img_num] = readin_image(readin_path)
+        flatten_diff_stack = frame_diff(flatten_img_stack,img_stack,use_flatten=True)
+        low_pass_video_sig = low_pass(flatten_img_stack, img_stack, dimx, dimy, True)
+        axis_x = np.linspace(0, low_pass_video_sig.shape[1], num=low_pass_video_sig.shape[1])
         plt.subplot(221)
-        # plt.imshow(seq_std_norm_thresh_inv,cmap='jet')
-        plt.imshow(frame_diff_seq_std_norm_thresh_inv)
-        plt.title('frame difference_otsu')
+        # 15860 21670 31253 22123
+        logo_point = 22123
+        non_logo_point = 4500
+        plt.plot(axis_x, flatten_img_stack[logo_point, :])
+        plt.title(u'logo original difference')
+        plt.axis('tight')
 
         plt.subplot(222)
-        plt.imshow(difference_sum)
-        plt.title('frame difference')
-
-
+        plt.plot(axis_x, low_pass_video_sig[logo_point, :])
+        plt.title(u'logo difference low pass')
+        plt.axis('tight')
 
         plt.subplot(223)
-        plt.imshow(seq_std_norm_thresh_inv)
-        plt.title('standard devivation_otsu')
+        plt.plot(axis_x, flatten_img_stack[non_logo_point, :])
+        plt.title(u'non-logo original difference')
+        plt.axis('tight')
 
         plt.subplot(224)
-        plt.imshow(seq_std)
-        plt.title('standard devivation')
-
-
+        plt.plot(axis_x, low_pass_video_sig[non_logo_point, :])
+        plt.title(u'non-logo difference low pass')
+        plt.axis('tight')
         plt.show()
-    else:
-        cv2.imshow('difference',seq_std_norm_thresh_inv)
-        cv2.waitKey(5000)
-        cv2.destroyWindow('difference')
+
+
 
 def flatten_img_freq_analysis():
     readin_path = '/Users/shichao/Downloads'
     [flatten_img_stack,img_stack,dimx,dimy,img_num] = readin_image(readin_path)
-    flatten_diff_stack = frame_diff(flatten_img_stack)
     low_pass_video_sig = low_pass(flatten_img_stack,img_stack,dimx,dimy,True)
-    low_pass_img_stack = low_pass_video_sig.reshape(dimx,dimy,img_num)
     axis_x = np.linspace(0, low_pass_video_sig.shape[1], num=low_pass_video_sig.shape[1])
-    # 频率为5Hz的正弦信号
     plt.subplot(221)
     # 15860 21670 31253 22123
     logo_point = 22123
@@ -158,8 +199,30 @@ def flatten_img_freq_analysis():
     #     cv2.waitKey(5000)
     #     cv2.destroyWindow('low_pass{0}'.format(i))
 
+def get_frame_diff(img_stack):
+    frame_diff_stack = img_stack
+    return frame_diff_stack
+
+
+def get_std(img_stack):
+    img_stack_std = img_stack.std(axis=2)
+    return img_stack_std
+
+def get_otsu(img):
+    pass
+
+def solution_frame_diff_std_otsu():
+    readin_path = ''
+    img_stack = readin_image(readin_path)
+    frame_diff_stack = get_frame_diff(img_stack)
+    frame_diff_stack_std = get_std(frame_diff_stack)
+
+
+
 def main():
     img_freq_analysis()
+
+    # flatten_img_freq_analysis()
 
 if __name__ == '__main__':
     main()
