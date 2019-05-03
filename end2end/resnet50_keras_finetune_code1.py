ab2db38f83efa10e6faccf4878adf38022d254ba
csrunner <chaoshi_cn@hotmail.com>
2019-05-03 16:01:53 +0800

    keras implementation of resnet50

---
+++ end2end/resnet50_keras_finetune_code1.py
@@ -0,0 +1,123 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+import keras
+from keras.preprocessing.image import ImageDataGenerator, load_img
+
+train_dir = '/home/shichao/data/TV_LOGO_TRAIN_VAL_TEST_tiny/train'
+validation_dir = '/home/shichao/data/TV_LOGO_TRAIN_VAL_TEST_tiny/val'
+image_size = 224
+
+from keras.applications.resnet50 import ResNet50
+
+#Load the VGG model
+resnet50_conv = ResNet50(weights='imagenet', include_top=False, input_shape=(image_size, image_size, 3))
+
+'''
+# Freeze all the layers
+for layer in resnet50_conv.layers[:-4]:
+    layer.trainable = False
+'''
+
+# Check the trainable status of the individual layers
+for layer in resnet50_conv.layers:
+    print(layer, layer.trainable)
+
+
+from keras import models
+from keras import layers
+from keras import optimizers
+
+# Create the model
+model = models.Sequential()
+
+# Add the vgg convolutional base model
+model.add(resnet50_conv)
+
+
+# Add new layers
+model.add(layers.Flatten())
+model.add(layers.Dense(1024, activation='relu'))
+model.add(layers.Dropout(0.5))
+model.add(layers.Dense(104, activation='softmax')) # N positive classes + 1 negative class
+
+
+# Show a summary of the model. Check the number of trainable parameters
+model.summary()
+
+# No Data augmentation
+train_datagen = ImageDataGenerator(rescale=1./255)
+validation_datagen = ImageDataGenerator(rescale=1./255)
+
+# Change the batchsize according to your system RAM
+train_batchsize = 32
+val_batchsize = 16
+
+# Data Generator for Training data
+train_generator = train_datagen.flow_from_directory(
+        train_dir,
+        target_size=(image_size, image_size),
+        batch_size=train_batchsize,
+        class_mode='categorical')
+
+# Data Generator for Validation data
+validation_generator = validation_datagen.flow_from_directory(
+        validation_dir,
+        target_size=(image_size, image_size),
+        batch_size=val_batchsize,
+        class_mode='categorical',
+        shuffle=False)
+
+# Compile the model
+model.compile(loss='categorical_crossentropy',
+              optimizer=optimizers.RMSprop(lr=1e-4),
+              metrics=['acc'])
+
+# Train the Model
+history = model.fit_generator(
+      train_generator,
+      steps_per_epoch=train_generator.samples/train_generator.batch_size ,
+      epochs=20,
+      validation_data=validation_generator,
+      validation_steps=validation_generator.samples/validation_generator.batch_size,
+      verbose=1)
+
+# Save the Model
+model.save('resnet50_last4_layers.h5')
+
+# Plot the accuracy and loss curves
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(len(acc))
+
+# Create a generator for prediction
+validation_generator = validation_datagen.flow_from_directory(
+        validation_dir,
+        target_size=(image_size, image_size),
+        batch_size=val_batchsize,
+        class_mode='categorical',
+        shuffle=False)
+
+# Get the filenames from the generator
+fnames = validation_generator.filenames
+
+# Get the ground truth from generator
+ground_truth = validation_generator.classes
+
+# Get the label to class mapping from the generator
+label2index = validation_generator.class_indices
+
+# Getting the mapping from class index to class label
+idx2label = dict((v,k) for k,v in label2index.items())
+
+# Get the predictions from the model using the generator
+predictions = model.predict_generator(validation_generator, steps=validation_generator.samples/validation_generator.batch_size,verbose=1)
+predicted_classes = np.argmax(predictions,axis=1)
+
+errors = np.where(predicted_classes != ground_truth)[0]
+print("No of errors = {}/{}".format(len(errors),validation_generator.samples))
+
+
