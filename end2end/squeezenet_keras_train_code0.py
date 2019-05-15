d42cf2d69812b9d9cf996658a2e68886a48fc001
csrunner <chaoshi_cn@hotmail.com>
2019-05-15 21:04:16 +0800

    squeezenet training code

---
+++ end2end/squeezenet_keras_train_code0.py
@@ -0,0 +1,261 @@
+from keras_applications.imagenet_utils import _obtain_input_shape
+from keras import backend as K
+from keras.layers import Input, Convolution2D, MaxPooling2D, Activation, concatenate, Dropout, warnings
+from keras.layers import GlobalAveragePooling2D, GlobalMaxPooling2D
+from keras.models import Model
+from keras.engine.topology import get_source_inputs
+from keras.utils import get_file
+from keras.utils import layer_utils
+
+
+sq1x1 = "squeeze1x1"
+exp1x1 = "expand1x1"
+exp3x3 = "expand3x3"
+relu = "relu_"
+
+WEIGHTS_PATH = "https://github.com/rcmalli/keras-squeezenet/releases/download/v1.0/squeezenet_weights_tf_dim_ordering_tf_kernels.h5"
+WEIGHTS_PATH_NO_TOP = "https://github.com/rcmalli/keras-squeezenet/releases/download/v1.0/squeezenet_weights_tf_dim_ordering_tf_kernels_notop.h5"
+
+# Modular function for Fire Node
+
+def fire_module(x, fire_id, squeeze=16, expand=64):
+    s_id = 'fire' + str(fire_id) + '/'
+
+    if K.image_data_format() == 'channels_first':
+        channel_axis = 1
+    else:
+        channel_axis = 3
+    
+    x = Convolution2D(squeeze, (1, 1), padding='valid', name=s_id + sq1x1)(x)
+    x = Activation('relu', name=s_id + relu + sq1x1)(x)
+
+    left = Convolution2D(expand, (1, 1), padding='valid', name=s_id + exp1x1)(x)
+    left = Activation('relu', name=s_id + relu + exp1x1)(left)
+
+    right = Convolution2D(expand, (3, 3), padding='same', name=s_id + exp3x3)(x)
+    right = Activation('relu', name=s_id + relu + exp3x3)(right)
+
+    x = concatenate([left, right], axis=channel_axis, name=s_id + 'concat')
+    return x
+
+
+# Original SqueezeNet from paper.
+
+def SqueezeNet(include_top=False, 
+               input_tensor=None, input_shape=None,
+               pooling=None,
+               classes=104):
+    """Instantiates the SqueezeNet architecture.
+    """
+    '''    
+    if weights not in {'imagenet', None}:
+        raise ValueError('The `weights` argument should be either '
+                         '`None` (random initialization) or `imagenet` '
+                         '(pre-training on ImageNet).')
+
+    if weights == 'imagenet' and classes != 1000:
+        raise ValueError('If using `weights` as imagenet with `include_top`'
+                         ' as true, `classes` should be 1000')
+
+    '''
+    input_shape = _obtain_input_shape(input_shape,
+                                      default_size=227,
+                                      min_size=48,
+                                      data_format=K.image_data_format(),
+                                      require_flatten=include_top)
+
+    if input_tensor is None:
+        img_input = Input(shape=input_shape)
+    else:
+        if not K.is_keras_tensor(input_tensor):
+            img_input = Input(tensor=input_tensor, shape=input_shape)
+        else:
+            img_input = input_tensor
+
+
+    x = Convolution2D(64, (3, 3), strides=(2, 2), padding='valid', name='conv1')(img_input)
+    x = Activation('relu', name='relu_conv1')(x)
+    x = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), name='pool1')(x)
+
+    x = fire_module(x, fire_id=2, squeeze=16, expand=64)
+    x = fire_module(x, fire_id=3, squeeze=16, expand=64)
+    x = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), name='pool3')(x)
+
+    x = fire_module(x, fire_id=4, squeeze=32, expand=128)
+    x = fire_module(x, fire_id=5, squeeze=32, expand=128)
+    x = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), name='pool5')(x)
+
+    x = fire_module(x, fire_id=6, squeeze=48, expand=192)
+    x = fire_module(x, fire_id=7, squeeze=48, expand=192)
+    x = fire_module(x, fire_id=8, squeeze=64, expand=256)
+    x = fire_module(x, fire_id=9, squeeze=64, expand=256)
+    
+    if include_top:
+        # It's not obvious where to cut the network... 
+        # Could do the 8th or 9th layer... some work recommends cutting earlier layers.
+    
+        x = Dropout(0.5, name='drop9')(x)
+
+        x = Convolution2D(classes, (1, 1), padding='valid', name='conv10')(x)
+        x = Activation('relu', name='relu_conv10')(x)
+        x = GlobalAveragePooling2D()(x)
+        x = Activation('softmax', name='loss')(x)
+    else:
+        if pooling == 'avg':
+            x = GlobalAveragePooling2D()(x)
+        elif pooling=='max':
+            x = GlobalMaxPooling2D()(x)
+        elif pooling==None:
+            pass
+        else:
+            raise ValueError("Unknown argument for 'pooling'=" + pooling)
+
+    # Ensure that the model takes into account
+    # any potential predecessors of `input_tensor`.
+    if input_tensor is not None:
+        inputs = get_source_inputs(input_tensor)
+    else:
+        inputs = img_input
+
+    model = Model(inputs, x, name='squeezenet')
+    return model
+
+
+
+
+import numpy as np
+#import matplotlib.pyplot as plt
+#%matplotlib inline
+#from __future__ import print_function
+import keras
+from keras.preprocessing.image import ImageDataGenerator, load_img
+import os
+
+# left top corner
+#train_dir = '/home/shichao/data/TV_LOGO_TRAIN_VAL_TEST_tiny/train'
+#validation_dir = '/home/shichao/data/TV_LOGO_TRAIN_VAL_TEST_tiny/val'
+# 4 corners merged
+
+image_size1 = 224
+image_size2 = 224
+epoch = 20
+
+root = '/home/shichao/mount-dir/data/t2000/lt_corner/data/TV_LOGO_TRAIN_VAL_TEST_tiny'
+train_dir = os.path.join(root,'train')
+validation_dir = os.path.join(root,'val')
+
+#train_dir = '/home/shichao/mount-dir/2/TV_LOGO_FULL_IMG_TRAIN_VAL_TEST_{0}_{1}/train'.format(image_size1,image_size2)
+#validation_dir = '/home/shichao/mount-dir/2/TV_LOGO_FULL_IMG_TRAIN_VAL_TEST_{0}_{1}/val'.format(image_size1,image_size2)
+#image_size = 224
+
+from keras.applications import VGG16
+
+#Load the VGG model
+#vgg_conv = VGG16(weights='imagenet', include_top=False, input_shape=(3,image_size1, image_size2))
+squeeze_conv = SqueezeNet()
+# Freeze all the layers
+#for layer in vgg_conv.layers[:-8]:
+#    layer.trainable = False
+
+# Check the trainable status of the individual layers
+#for layer in vgg_conv.layers:
+#    print(layer, layer.trainable)
+#for layer in vgg_conv.layers:
+#    print(layer.name)
+
+from keras import models
+from keras import layers
+from keras import optimizers
+
+# Create the model
+model = models.Sequential()
+
+# Add the vgg convolutional base model
+model.add(squeeze_conv)
+
+# Add new layers
+#model.add(layers.Flatten())
+#model.add(layers.Dense(1024, activation='relu'))
+#model.add(layers.Dropout(0.5))
+model.add(layers.GlobalMaxPooling2D())
+model.add(layers.Dense(104, activation='softmax')) # 102 positive classes + 1 negative class
+#print([layer.name for layer in model.get_layer('vgg16').layers])
+print([layer.name for layer in model.layers])
+# Show a summary of the model. Check the number of trainable parameters
+model.summary()
+
+
+
+train_datagen = ImageDataGenerator(rescale=1./255)
+validation_datagen = ImageDataGenerator(rescale=1./255)
+
+# Change the batchsize according to your system RAM
+train_batchsize = 128
+val_batchsize = 128
+
+# Data Generator for Training data
+train_generator = train_datagen.flow_from_directory(
+        train_dir,
+        target_size=(image_size1, image_size2),
+        batch_size=train_batchsize,
+        class_mode='categorical')
+
+# Data Generator for Validation data
+validation_generator = validation_datagen.flow_from_directory(
+        validation_dir,
+        target_size=(image_size1, image_size2),
+        batch_size=val_batchsize,
+        class_mode='categorical',
+        shuffle=False)
+
+# Compile the model
+model.compile(loss='categorical_crossentropy',
+              optimizer=optimizers.RMSprop(lr=1e-4),
+              metrics=['acc'])
+
+# Train the Model
+history = model.fit_generator(
+      train_generator,
+      steps_per_epoch=train_generator.samples/train_generator.batch_size ,
+      epochs=epoch,
+      validation_data=validation_generator,
+      validation_steps=validation_generator.samples/validation_generator.batch_size,
+      verbose=1)
+
+# Save the Model
+model.save('squeeze_104_classes_ltcorner_nhwc_epoch{0}_batch{1}.h5'.format(epoch,train_batchsize))
+
+# Plot the accuracy and loss curves
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(len(acc))
+
+
+Validation_generator = validation_datagen.flow_from_directory(
+        validation_dir,
+        target_size=(image_size1, image_size2),
+        batch_size=val_batchsize,
+        class_mode='categorical',
+        shuffle=False)
+
+#Get the filenames from the generator
+fnames = validation_generator.filenames
+
+# Get the ground truth from generator
+ground_truth = validation_generator.classes
+
+# Get the label to class mapping from the generator
+label2index = validation_generator.class_indices
+
+# Getting the mapping from class index to class label
+idx2label = dict((v,k) for k,v in label2index.items())
+
+# Get the predictions from the model using the generator
+predictions = model.predict_generator(validation_generator, steps=validation_generator.samples/validation_generator.batch_size,verbose=1)
+predicted_classes = np.argmax(predictions,axis=1)
+
+errors = np.where(predicted_classes != ground_truth)[0]
+print("No of errors = {}/{}".format(len(errors),validation_generator.samples))
